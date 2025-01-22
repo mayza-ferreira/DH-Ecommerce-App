c8613dac04eecbc410638b6cb3af3054d01b32f6
Mayza Ferreira <mayza_ferr@hotmail.com>
2025-01-22 17:45:26 -0300

    feat: initial project setup with router configuration

---
+++ Ecommerce/src/main.tsx
@@ -0,0 +1,25 @@
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { LayoutMain } from "./Components/Layouts/LayoutMain.tsx";
+import "./index.css";
+import Cart from "./pages/Cart/Cart.tsx";
+import Home from "./pages/Home/Home.tsx";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <LayoutMain />,
+    children: [
+      { index: true, element: <Home /> },
+      { path: "/cart", element: <Cart /> },
+    ],
+  },
+]);
+
+createRoot(document.getElementById("root")!).render(
+  <StrictMode>
+    {/* <App /> */}
+    <RouterProvider router={router} />
+  </StrictMode>
+);
