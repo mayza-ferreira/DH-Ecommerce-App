c5c87cabd84bf6873bc03c8f182e68fd719d060c
Mayza Ferreira <mayza_ferr@hotmail.com>
2025-01-22 19:31:22 -0300

    feat: add product fetching logic for Home page

--- Ecommerce/src/pages/Home/Home.tsx
+++ Ecommerce/src/pages/Home/Home.tsx
@@ -1,8 +1,28 @@
+import { useEffect, useState } from "react";
+import { Hero } from "../../Components/ui/Hero/Hero";
 
 const Home = () => {
-  return (
-    <div>Home</div>
-  )
-}
+  const [products, setProducts] = useState([]);
+  const getProducts = async () => {
+    try {
+      const response = await fetch("http://localhost:3000/products");
+      const data = await response.json();
+      setProducts(data);
+    } catch (error) {
+      console.log(error);
+    }
+  };
 
-export default Home
+  useEffect(() => {
+    getProducts();
+  }, []);
+
+  console.log(products);
+  return (
+    <>
+      <Hero />
+    </>
+  );
+};
+
+export default Home;
