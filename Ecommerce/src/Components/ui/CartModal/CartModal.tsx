1b4ba7a2dd0ac19b4ae2741e07d97fa7903af10d
Mayza Ferreira <mayza_ferr@hotmail.com>
2025-01-23 11:50:37 -0300

    feat: add CartModal component with styles and integrate it into Navbar

---
+++ Ecommerce/src/Components/ui/CartModal/CartModal.tsx
@@ -0,0 +1,41 @@
+import styles from "./CartModal.module.css";
+import Close from "../../../assets/close.svg";
+const CartModal = (handleShowCartModal) => {
+  return (
+    <div className={styles.modalContainer}>
+      <button className={styles.modalCloseButton} onClick={handleShowCartModal}>
+        <img src={Close} alt="Close" />
+      </button>
+      <table className={styles.modalTable}>
+        <thead>
+          <tr>
+            <th>Product</th>
+            <th>Delete</th>
+            <th>Quantity</th>
+            <th>Add</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>name</td>
+            <td>
+              <button className={styles.modalButtonRemove}>-</button>
+            </td>
+            <td>12</td>
+            <td>
+              <button className={styles.modalButtonAdd}>+1</button>
+            </td>
+          </tr>
+        </tbody>
+      </table>
+      <div className={styles.modalTotalContainer}>
+        <h3>Total: 400,00</h3>
+      </div>
+      <div className={styles.modalButtonContainer}>
+        <button>Checkout</button>
+      </div>
+    </div>
+  );
+};
+
+export default CartModal;
--- Ecommerce/src/Components/ui/Navbar/Navbar.tsx
+++ Ecommerce/src/Components/ui/Navbar/Navbar.tsx
@@ -1,7 +1,13 @@
 import Logo from "../../../assets/logo.svg";
 import cartIcon from "../../../assets/cart.svg";
 import styles from "./Navbar.module.css";
+import { useState } from "react";
+import CartModal from "../CartModal/CartModal";
 export const Navbar = () => {
+  const [showCartModal, setShowCartModal] = useState(false);
+  const handleShowCartModal = () => {
+    setShowCartModal(!showCartModal);
+  };
   return (
     <div className={styles.navbarContainer}>
       <div className={styles.navbarDetail}>
@@ -12,8 +18,9 @@ export const Navbar = () => {
       </div>
       <div className={styles.navbarCartContainer}>
         <p className={styles.navbarTextAmount}>2</p>
-        <img src={cartIcon} alt="Cart" />
+        <img src={cartIcon} alt="Cart" onClick={handleShowCartModal} />
       </div>
+      {showCartModal && <CartModal handleShowCartModal={handleShowCartModal} />}
     </div>
   );
 };
