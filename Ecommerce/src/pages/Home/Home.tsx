362c911d81b1f40e9829c27c64336098e79b3946
Mayza Ferreira <mayza_ferr@hotmail.com>
2025-01-28 23:59:29 -0300

    feat: Add Pagination with react query

--- Ecommerce/src/pages/Home/Home.tsx
+++ Ecommerce/src/pages/Home/Home.tsx
@@ -4,20 +4,50 @@ import { CardProduct } from "../../Components/ui/CardProduct";
 import { Hero } from "../../Components/ui/Hero";
 import { getProducts } from "../../service";
 import styles from "./Home.module.css";
+import { useState } from "react";
 
 const Home = () => {
-  const { data, isLoading, error } = useQuery("products", getProducts);
+  const [page, setPage] = useState(1);
+
+  const { data, isLoading, error } = useQuery(
+    ["products", page],
+    () => getProducts(page),
+    { keepPreviousData: true }
+  );
+
+  const handleNextPage = () => {
+    setPage((prevPage) => prevPage + 1);
+  };
+
+  const handlePreviousPage = () => {
+    setPage((prevPage) => prevPage - 1);
+  };
 
   return (
     <>
       <Hero />
       <Toaster richColors />
       {isLoading && <p>Loading...</p>}
-      {error && <p>Something went wrong :(</p>}
+      {error && <p>Something went wrong</p>}
       <div className={styles.container}>
-        {data?.map((product) => (
+        {data?.data?.map((product) => (
           <CardProduct key={product.id} product={product} />
         ))}
+      </div>
+      <div className={styles.paginationContainer}>
+        <button
+          onClick={handlePreviousPage}
+          className={styles.paginationButton}
+          disabled={page === 1}
+        >
+          previus page
+        </button>
+        <div className={styles.paginationActive}>
+          <span>{page}</span>
+        </div>
+        <button className={styles.paginationButton} onClick={handleNextPage}>
+          next page
+        </button>
       </div>
     </>
   );
--- Ecommerce/src/service/products.service.ts
+++ Ecommerce/src/service/products.service.ts
@@ -1,8 +1,11 @@
 import { Product } from "../interface";
 
-export const getProducts = async (): Promise<Product[]> => {
+export const getProducts = async (page = 0): Promise<Product[]> => {
   try {
-    const response = await fetch("http://localhost:3000/products");
+    const response = await fetch(
+      // `http://localhost:3000/products?_page=${page}&_limit=24`
+      `http://localhost:3000/products?_page=${page}&_per_page=24`
+    );
     if (response.ok) {
       const data = await response.json();
       return data;
